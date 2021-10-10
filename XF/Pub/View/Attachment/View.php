31e69ed6b8c575127aca0f2f803ee91c4209525b
Luu Truong <truonglv@outlook.com>
2021-10-10 21:59:02 +0700

    Add cache directive

---
+++ XF/Pub/View/Attachment/View.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Truonglv\ImageCacheable\XF\Pub\View\Attachment;
+
+use XF\Util\File;
+
+class View extends XFCP_View
+{
+    /**
+     * @return string
+     */
+    public function renderRaw()
+    {
+        /** @var \XF\Entity\Attachment $attachment */
+        $attachment = $this->params['attachment'];
+        $return304 = (bool) $this->params['return304'];
+        if (!$return304
+            && File::isImageInlineDisplaySafe($attachment->extension)
+        ) {
+            $this->response->header('Cache-Control', 'public, must-revalidate, max-age=' . (30 * 86400));
+        }
+
+        return parent::renderRaw();
+    }
+}
---
+++ _output/extension_hint.php
@@ -0,0 +1,14 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace Truonglv\ImageCacheable\XF\Api\View\Attachment
+{
+	class XFCP_View extends \XF\Api\View\Attachment\View {}
+}
+
+namespace Truonglv\ImageCacheable\XF\Pub\View\Attachment
+{
+	class XFCP_View extends \XF\Pub\View\Attachment\View {}
+}
